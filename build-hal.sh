1a23802babb1a8bf3e5d9ddb4b3dde45260da6c3
handsomeyingyan <handsomeyingyan@github.com>
2020-05-14 19:05:14 +0800

    add basic files

---
+++ build-hal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source halium.env
+cd $ANDROID_ROOT
+
+source build/envsetup.sh
+export USE_CCACHE=1
+breakfast $DEVICE
+make -j$(nproc) halium-boot
+make -j$(nproc) recoveryimage
+make -j$(nproc) systemimage 
+
+echo "md5sum halium-boot.img and system.img"
+md5sum $ANDROID_ROOT/out/target/product/kenzo/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/kenzo/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/kenzo/system.img
