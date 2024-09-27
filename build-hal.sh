bd4a20f5acc1f891b6f02eb40308f2444804d217
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2024-09-27 21:04:43 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf .repo
 source halium.env
 cd $ANDROID_ROOT
 
@@ -7,9 +8,9 @@ export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) halium-boot
 make -j$(nproc) recoveryimage
-make -j$(nproc) systemimage 
+#make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/lmi/halium-boot.img
 md5sum $ANDROID_ROOT/out/target/product/lmi/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/lmi/system.img
+#md5sum $ANDROID_ROOT/out/target/product/lmi/system.img
