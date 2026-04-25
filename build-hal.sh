fc4aa5fc3dffd4013c0737a56dc0b1d45b312d4f
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2026-04-25 11:04:30 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -2,20 +2,9 @@
 rm -rf .repo
 source halium.env
 cd $ANDROID_ROOT
+sed -i '/curl --location/c\ python3 - <<EOF\n\ import urllib.request, ssl, sys\n\ url = "'"$LINK_FULL"'"\n\ out = "'"$TARGET"'"\n\ ctx = ssl.create_default_context()\n\ ctx.check_hostname = False\n\ ctx.verify_mode = ssl.CERT_NONE\n\ with urllib.request.urlopen(url, context=ctx) as r:\n\     data = r.read()\n\ open(out, "wb").write(data)\n\ EOF' halium/halium-boot/get-initrd.sh
 
 source build/envsetup.sh
-sed -i '/curl --location/c\
-python3 - <<EOF\n\
-import urllib.request, ssl, sys\n\
-url = "'"$LINK_FULL"'"\n\
-out = "'"$TARGET"'"\n\
-ctx = ssl.create_default_context()\n\
-ctx.check_hostname = False\n\
-ctx.verify_mode = ssl.CERT_NONE\n\
-with urllib.request.urlopen(url, context=ctx) as r:\n\
-    data = r.read()\n\
-open(out, "wb").write(data)\n\
-EOF' halium/halium-boot/get-initrd.sh
 #export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) halium-boot
