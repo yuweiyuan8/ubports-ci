a0efc6cc833676470c62efa45f651fd6cb57e518
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2026-04-27 22:45:28 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -3,8 +3,6 @@ rm -rf .repo
 source halium.env
 cd $ANDROID_ROOT
 export LINK_FULL TARGET
-sed -i '/curl --location/c\python3 -c '\''import urllib.request,ssl,os;ctx=ssl.create_default_context();ctx.check_hostname=False;ctx.verify_mode=ssl.CERT_NONE;urllib.request.urlretrieve(os.environ["LINK_FULL"], os.environ["TARGET"])'\''' halium/halium-boot/get-initrd.sh
-
 source build/envsetup.sh
 #export USE_CCACHE=1
 breakfast $DEVICE
